982a1c210c07add11802284ecfc266b23179064d
ann4703-art <ann4703@g.rit.edu>
2026-09-01 04:16:02 -0400

    Add shebang and make  syshealth.sh executable

---
+++ syshealth.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#=====================
+# syshealth.sh - System Health & Log Analysis Toolkit
+# lab 1 - Data collector
+#Author :Abdulaziz Nasser
+#Date: $(date +%Y-%m-%d)
+#=========================
