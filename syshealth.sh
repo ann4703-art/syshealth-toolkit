df85df42d3d7d2c0c4de0bf3d8ddfb739a2a36d8
ann4703-art <ann4703@g.rit.edu>
2026-09-01 04:50:39 -0400

    Demostrate quoting rules with explanatory comment

--- syshealth.sh
+++ syshealth.sh
@@ -6,3 +6,17 @@
 #Author :Abdulaziz Nasser
 #Date: $(date +%Y-%m-%d)
 #=========================
+ 
+#===============
+HOSTNAME=$(hostname)
+CURRENT_DATE=$(date '+%Y-%m-%d %H:%M:%S')
+# Important: Quoting demo (Python/java students read this!)
+#Without quotes -> word-splitting bug(try it!)
+#With double quotes -> safe (Bash best practice)
+echo "Hostname without quotes:$HOSTNAME"
+echo "Hostname with quotes: \"$HOSTNAME\""
+cat << EOF
+# COMMENT FOR GRADER:
+#In my python/java variable expand safely.
+#In Bash, unqouted \$VAR splits on spaces/tabs/newlines.
+#Always double-quote unless you debliberately want splitting EOF
